4a7e80b42d8c5577c0086c2ac803556c543756da
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-07-24 23:45:43 +0530

    More Binary Tree Q's

--- binary_Tree4.cpp
+++ binary_Tree4.cpp
@@ -30,6 +30,105 @@ int sumRootToLeaf(Node* root){
     return maxSum;
 }
 
+//Lowest common ancestor of a Binary Tree
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return NULL;
+        }
+        
+        if(root->val==p->val || root->val== q->val){
+            return root;
+        }
+        
+        TreeNode* leftAns= lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightAns= lowestCommonAncestor(root->right,p,q);
+        
+        if(leftAns!=NULL && rightAns!=NULL){
+            return root;
+        }else if(leftAns!=NULL && rightAns==NULL){
+            return leftAns;
+        }else if(leftAns==NULL && rightAns!=NULL){
+            return rightAns;
+        }else{
+            return NULL;
+        }
+        
+    }
+
+ // K Sum Path in a Binary Tree
+
+ void solve2(Node* root, int k, int& count, vector<int> path){
+    if(root==NULL){
+        return;
+    }
+
+    path.push_back(root->data);
+
+    solve2(root->left,k,count,path);
+    solve2(root->right,k,count,path);
+
+    int size= path.size();
+    int sum=0;
+    for(int i=size-1;i>=0;i--){
+            sum+=path[i];
+            if(sum==k) count++;
+    }
+ }
+
+ int sumK(Node* root, int k){
+    vector<int> path;
+    int count=0;
+
+    solve2(root,k,count,path);
+    return count;
+ }
+
+
+
+ // Kth Ancestor in a Tree
+
+Node* solve3(Node* root, int&k ,int node){
+    if(root==NULL) return NULL;
+
+    if(root->data==node){
+        return root;
+    }
+
+
+    Node* left= solve3(root->left,k,node);
+    Node* right= solve3(root->right,K,node);
+
+    if(left!=NULL && right==NULL){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }else{
+            return left;
+        }
+    }
+    else if(left==NULL && right!=NULL){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }else{
+            return right;
+        }
+    }
+    return NULL;
+}
+
+int kthAncestor(Node* root, int k,int node){
+
+    Node* ans= solve3(root,k,node);
+    if(ans==NULL) return -1;
+    else return ans->data;
+}
+
+//
+
+
 int main(){
      
 
