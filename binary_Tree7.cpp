b5f81b093ba47f0b947b634866bbe74ec49c697f
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-07-26 18:57:31 +0530

    Flattten a binary Tree to Linked List

---
+++ binary_Tree7.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std; 
+// Flatten a binary tree to linked list
+
+
+/*
+void flatten(Node* root){
+    Node* curr=root;
+    while(curr!=NULL){
+        if(curr->left){
+            Node* prev= curr->left;
+            while(prev->right){
+                prev=prev->right;
+            }
+
+            prev->right=curr->right;
+            curr->right=curr->left;
+            curr->left=NULL;
+        }
+        curr=curr->right;
+    }
+}
+
+
+
+*/
+int main(){
+     
+
+    return 0;
+}
