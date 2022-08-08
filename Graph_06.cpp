134607b44281665427c4dd597441eeb6b415ff05
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-08-08 11:27:38 +0530

    Topo Sort

--- Graph_06.cpp
+++ Graph_06.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 
 // Topological Sort on  DAG(Directed Acyclic Graph)
+//Using DFS
+
 void dfs(unordered_map<int, set<int> > &adjList, unordered_map<int,bool> &visited, int node, stack<int> &s){
     visited[node]=1;
     
@@ -37,6 +39,38 @@ vector<int> topologicalSort(vector<vector<int>> &graph, int edges, int nodes) {
     }
     return ans;
 }
+
+//Using BFS (Kahns Algorithm)
+vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	   vector<int> in_degree(V,0);
+	  for(int i=0;i<V;i++){
+	      for(int j=0;j<adj[i].size();j++){
+	          in_degree[adj[i][j]]++;
+	      }
+	  }
+	  queue<int> q;
+    for (int i = 0; i < V; i++)
+        if (in_degree[i] == 0)
+            q.push(i);
+     
+    vector<int> ans;
+    
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        
+        ans.push_back(front);
+        
+        for(auto i: adj[front]){
+            if(--in_degree[i]==0){
+                q.push(i);
+            }
+        }
+    }
+    return ans;
+	}
+
 int main(){
      
 
