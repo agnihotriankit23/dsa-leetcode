7fde29012489d99b00961c0de0df7cf762780b2e
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-08-18 11:04:43 +0530

    Max Sum without adjacents

---
+++ DP_03.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// MAX SUM WITHOUT ADJACENTS
+class Solution
+{
+public:
+    // calculate the maximum sum with out adjacent
+    int solve(int *arr, int n, vector<long long int> &dp)
+    {
+        if (n < 0)
+            return 0;
+
+        if (dp[n] != -1)
+            return dp[n];
+        int include = solve(arr, n - 2, dp) + arr[n];
+        int exclude = solve(arr, n - 1, dp) + 0;
+
+        dp[n] = max(include, exclude);
+        return dp[n];
+    }
+    int findMaxSum(int *arr, int n)
+    {
+        // code here
+        vector<long long int> dp(n + 1, -1);
+        dp[0] = arr[0];
+        int ans = solve(arr, n - 1, dp);
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
