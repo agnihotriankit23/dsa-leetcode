e49a01bb4e13b05df9a4850a448d11d1ec3dea49
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-08-25 09:54:41 +0530

    REDUCING DISHES LEETCODE

---
+++ DP_14.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Reducing Dishes
+class Solution
+{
+public:
+    int solve(vector<int> &satisfaction, int index, int time)
+    {
+
+        if (index == satisfaction.size())
+            return 0;
+
+        int include = satisfaction[index] * (time + 1) + solve(satisfaction, index + 1, time + 1);
+        int exclude = 0 + solve(satisfaction, index + 1, time);
+
+        return max(exclude, include);
+    }
+
+    // This is TOP-DOWN Approach
+    int solveMem(vector<int> &satisfaction, int index, int time, vector<vector<int>> &dp)
+    {
+
+        if (index == satisfaction.size())
+            return 0;
+
+        if (dp[index][time] != -1)
+            return dp[index][time];
+        int include = satisfaction[index] * (time + 1) + solveMem(satisfaction, index + 1, time + 1, dp);
+        int exclude = 0 + solveMem(satisfaction, index + 1, time, dp);
+
+        return dp[index][time] = max(exclude, include);
+    }
+
+    // This is BOTTOM-UP Approach
+    int solveTab(vector<int> &satisfaction)
+    {
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int index = n - 1; index >= 0; index--)
+        {
+            for (int time = index; time >= 0; time--)
+            {
+
+                int include = satisfaction[index] * (time + 1) + dp[index + 1][time + 1];
+                int exclude = 0 + dp[index + 1][time];
+
+                dp[index][time] = max(include, exclude);
+            }
+        }
+        return dp[0][0];
+    }
+
+    // This is Space Optimised Approach
+    int solveSO(vector<int> &satisfaction)
+    {
+        int n = satisfaction.size();
+
+        vector<int> curr(n + 1, 0);
+        vector<int> next(n + 1, 0);
+
+        for (int index = n - 1; index >= 0; index--)
+        {
+            for (int time = index; time >= 0; time--)
+            {
+
+                int include = satisfaction[index] * (time + 1) + next[time + 1];
+                int exclude = 0 + next[time];
+
+                curr[time] = max(include, exclude);
+            }
+            next = curr;
+        }
+        return curr[0];
+    }
+    int maxSatisfaction(vector<int> &satisfaction)
+    {
+        // int ans = solve(satisfaction,0,0);
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        sort(satisfaction.begin(), satisfaction.end());
+        // return solveMem(satisfaction,0,0,dp);
+        //  return solveTab(satisfaction);
+
+        return solveSO(satisfaction);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
