664757cdba5823e1ac5cac50ea4d36958183d419
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-09-03 10:30:28 +0530

    Longest Arithmetic Subsequences Of Given Difference

---
+++ DP_23.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Longest Arithmetic Subsequence of Given Difference
+class Solution
+{
+public:
+    int longestSubsequence(vector<int> &arr, int difference)
+    {
+        unordered_map<int, int> dp;
+
+        int ans = 0;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int temp = arr[i] - difference;
+            int tempAns = 0;
+
+            if (dp.count(temp))
+                tempAns = dp[temp];
+
+            dp[arr[i]] = 1 + tempAns;
+
+            ans = max(ans, dp[arr[i]]);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
